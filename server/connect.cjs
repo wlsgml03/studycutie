416bc019de1e1b0865a6f2b8146c76069e68fe4e
wlsgml03 <jinheechang0329@gmail.com>
2025-06-20 15:52:11 -0400

    connecting database

---
+++ server/connect.cjs
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: "config.env" });
+
+async function main() {
+  const Db = process.env.ATLAS_URI;
+  const client = new MongoClient(Db);
+
+  try {
+    await client.connect();
+    const collections = await client.db("studyapp").collections();
+    collections.forEach((collection) =>
+      console.log(collection.s.namespace.collection)
+    );
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main();
